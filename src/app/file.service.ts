2ee5e7ff7dc53fb75388c20f72f0086d33889cd0
Manish Sharma <msmanishsharma52@gmail.com>
2021-07-15 20:14:25 +0530

    adding xlsx package

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import * as _ from 'lodash';
+
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +17,7 @@ export class FileService {
   constructor(private http: HttpClient) { }
 
   baseUrl = "http://13.126.188.182:5000/"
+
 
   private _userInfo = new BehaviorSubject<any>(null);
   private _isLoggingOut = new BehaviorSubject<any>(null)
@@ -55,5 +63,22 @@ export class FileService {
   downloadFileJson(fileId: any): Observable<any> {
     const url = this.baseUrl + 'get-file-data?file_id=' + fileId;
     return this.http.get(url)
+  }
+
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    console.log('worksheet', worksheet);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    //const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 }
--- src/app/processed-files/processed-files.component.ts
+++ src/app/processed-files/processed-files.component.ts
@@ -63,40 +63,18 @@ export class ProcessedFilesComponent implements OnInit {
 
   downloadFileJson(id: any, fileName: any) {
     this.fileServ.downloadFileJson(id).subscribe(data => {
-      let csvData = this.convertToCSV(data);
-      let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
-      let dwldLink = document.createElement("a");
-      let url = URL.createObjectURL(blob);
-      let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
-      if (isSafariBrowser) {
-        dwldLink.setAttribute("target", "_blank");
+      for (let i = 0; i < data.length; i++) {
+        let headers = Object.keys(data[i]);
+        for (let j = 0; j < headers.length; j++) {
+          let value = data[i][headers[j]]
+          if (value && typeof value == 'object') {
+            data[i][headers[j]] = value.join(', ')
+          }
+        }
       }
-      dwldLink.setAttribute("href", url);
-      dwldLink.setAttribute("download", fileName.split('.')[0] + ".csv");
-      dwldLink.style.visibility = "hidden";
-      document.body.appendChild(dwldLink);
-      dwldLink.click();
-      document.body.removeChild(dwldLink);
+      this.fileServ.exportAsExcelFile(data, fileName.split('.')[0])
     })
   }
 
-  convertToCSV(fileData: any) {
-    let headerList = Object.keys(fileData[0]); let str = '';
-    let row = 'S.No,';
-    for (let index in headerList) {
-      row += headerList[index] + ',';
-    }
-    row = row.slice(0, -1);
-    str += row + '\r\n';
-    for (let i = 0; i < fileData.length; i++) {
-      let line = (i + 1) + '';
-      for (let index in headerList) {
-        let head = headerList[index];
-        line += ',' + fileData[i][head];
-      }
-      str += line + '\r\n';
-    }
-    return str;
-  }
 
 }
